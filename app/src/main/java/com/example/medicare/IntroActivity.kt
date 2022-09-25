2c6660ccdc909bb1a1b71bc438a54b7ced1112d7
chunminkyung <cmk000907@naver.com>
2022-09-25 16:43:00 +0900

    0925 | minkyungchun | before modifying the grable file

--- app/src/main/java/com/example/medicare/IntroActivity.kt
+++ app/src/main/java/com/example/medicare/IntroActivity.kt
@@ -4,21 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.widget.ImageView
 
 class IntroActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState:Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.intro_activity)
+        setContentView(R.layout.activity_intro)
 
         SystemClock.sleep(3000) //3초 후 화면 전환
-        val intent=Intent(this,MediCalendar::class.java) //3초 뒤 넘어가는 페이지 지정
-        startActivity(intent) //MainActivity로 이동
+        val intent = Intent(this, MediCalendar::class.java)
+        startActivity(intent) //내가 지정한 페이지로 이동
         finish()
     }
-
-    override fun onPause() {
-        super.onPause()
-        finish()
-    }
-
 }
---
+++ app/src/main/java/com/example/medicare/MenuActionbar.kt
@@ -0,0 +1,6 @@
+package com.example.medicare
+
+class MenuActionbar {
+
+
+}
