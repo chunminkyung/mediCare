c5db4b73b303b62e58538fe80176bb75160805c9
chunminkyung <cmk000907@naver.com>
2022-09-25 19:44:26 +0900

    0925 | minkyungchun | intro screen success

---
+++ app/src/main/java/com/example/medicare/IntroActivity.java
@@ -0,0 +1,34 @@
+package com.example.medicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.ktmedicare.MainActivity;
+import com.example.ktmedicare.MediCalendar;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent (getApplicationContext(), MediCalendar.class);
+                startActivity(intent); //인트로 실행 후 바로 MainActivity로 넘어감.
+                finish();
+            }
+        },3000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
