3b8edb6f736ba6d90586b01d30459715d504c957
chunminkyung <cmk000907@naver.com>
2022-09-27 15:15:24 +0900

    0927 | minkyungchun | Calendar onDateDecorator Color complete

--- app/src/main/java/com/example/medicare/MediCalendar.java
+++ app/src/main/java/com/example/medicare/MediCalendar.java
@@ -1,16 +1,21 @@
 package com.example.medicare;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.medicare.databinding.ActivityMediCalendarBinding;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import java.util.Calendar;
+import java.util.List;
 
 public class MediCalendar extends AppCompatActivity {
     private ActivityMediCalendarBinding binding;
@@ -32,8 +37,16 @@ public class MediCalendar extends AppCompatActivity {
 
         materialCalendarView.addDecorators(
                 new SundayDecorator(),
-                new SaturdayDecorator()
-                //new onDayDecorator()
+                new SaturdayDecorator(),
+                new onDateDecorator()
         );
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+            }
+        });
     }
+
 }
