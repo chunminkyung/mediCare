6f07bb3639410738768a3b875b5218e342b3a921
chunminkyung <cmk000907@naver.com>
2022-09-27 14:16:39 +0900

    0927 | minkyungchun | Calendar Sunday, Saturday Decorator Color complete

--- app/src/main/java/com/example/medicare/MediCalendar.java
+++ app/src/main/java/com/example/medicare/MediCalendar.java
@@ -1,16 +1,39 @@
 package com.example.medicare;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.medicare.databinding.ActivityMediCalendarBinding;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
+
 public class MediCalendar extends AppCompatActivity {
+    private ActivityMediCalendarBinding binding;
+    //public MaterialCalendarView materialCalendarView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medi_calendar);
+        binding = ActivityMediCalendarBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        MaterialCalendarView materialCalendarView=binding.calendarView;
 
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2017, 0, 1))
+                .setMaximumDate(CalendarDay.from(2030, 11, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
 
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator()
+                //new onDayDecorator()
+        );
     }
 }
